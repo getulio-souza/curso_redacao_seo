598a2843b08959663ca1f555504c86d3a9a10d7e
Getúlio Souza <getuliosouza@live.com>
2022-06-26 23:08:55 -0300

    ajustando styled components

--- src/Components/Intro/intro.style.jsx
+++ src/Components/Intro/intro.style.jsx
@@ -9,7 +9,6 @@ export const IntroContainer = Styled.div`
 `;
 
 export const FirstContent = Styled.div`
-   /* max-width:50%; */
    display: flex;
    gap: 4rem;
    justify-content: center;
@@ -17,14 +16,7 @@ export const FirstContent = Styled.div`
    margin: 4rem 8rem;
    `;
 
-export const SecondContent = Styled.div`
-   /* max-width:50%; */
-   display: flex;
-   gap: 4rem;
-   justify-content: center;
-   align-items: center;
-   margin: 4rem 8rem;
-`;
+export const SecondContent = Styled(FirstContent)``;
 
 export const FirstContentText = Styled.h4`
   text-align:center;
@@ -35,14 +27,7 @@ export const FirstContentText = Styled.h4`
   display: block;
   `;
 
-export const SecondContentText = Styled.h4`
-  text-align:center;
-  font-size:20px;
-  font-weight:700;
-  line-height:1.7rem;
-  font-family: inter;
-  display: block;
-`;
+export const SecondContentText = Styled(FirstContentText)``;
 
 export const FirstContentImg = Styled.img`
    width: 500px;
@@ -51,11 +36,6 @@ export const FirstContentImg = Styled.img`
    box-shadow: 2px 5px 7px #000;
   `;
 
-export const SecondContentImg = Styled.img`
-   width: 500px;
-   height:auto;
-   border-radius: 20px;
-   box-shadow: 2px 5px 7px #000;
-`;
+export const SecondContentImg = Styled(FirstContentImg)``;
 
 
