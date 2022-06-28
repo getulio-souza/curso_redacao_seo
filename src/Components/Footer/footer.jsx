ec326e1e780bb2508b2d96c72ea56055ed5ef8b7
Getúlio Souza <getuliosouza@live.com>
2022-06-28 16:39:55 -0300

    footer finalizado

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,39 +1,68 @@
 import React from "react";
 import {
-    FooterContainer,
-    FooterIntro,
-    FooterLogo,
-    FooterText,
-    FooterSocialIcons,
-    FooterCategories,
-    FooterCategoryTitle,
-    FooterCategory,
+  FooterContainer,
+  FooterIntro,
+  FooterLogo,
+  FooterText,
+  FooterSocialIcons,
+  FooterSocialIcon,
+  FooterCategories,
+  FooterCategoryTitle,
+  FooterCategory,
+  FooterCategoryBtn,
+  FooterFormContainer,
+  FooterForm,
+  FooterFormTitle,
+  FooterFormLabel,
+  FooterLabelTextArea,
+  FooterFormBtn,
 } from "./footer.style";
 
-import FooterLogoImg from "../../assets/logo-3.JPG"
+import FooterLogoImg from "../../assets/logo-3.JPG";
+import FacebookIcon from "../../assets/social/facebook.png";
+import InstagramIcon from "../../assets/social/instagram.png";
+import TwitterIcon from "../../assets/social/twitter.png";
+import YoutubeIcon from "../../assets/social/youtube.png";
 
 function Footer() {
   return (
-      <FooterContainer>
-          {/* primeira parte */}
-          <FooterIntro>
-          <FooterLogo src={FooterLogoImg}></FooterLogo>
-          <FooterText>Este site foi criado para democratizar a formação de produtores de conteúdo para SEO que desejam se especializar na área.</FooterText>
-          <FooterSocialIcons>
-              <li>facebook</li>
-              <li>Instagram</li>
-              <li>Twitter</li>
-              <li>Youtube</li>
-      </FooterSocialIcons>
-          </FooterIntro>
-          {/* Categorias */}
-          <FooterCategories>
-              <FooterCategoryTitle>Categorias</FooterCategoryTitle>
-              <FooterCategory></FooterCategory>
-              <FooterCategory></FooterCategory>
-              <FooterCategory></FooterCategory>
-              <FooterCategory></FooterCategory>
-          </FooterCategories>
+    <FooterContainer>
+      <FooterIntro>
+        <FooterLogo src={FooterLogoImg}></FooterLogo>
+        <FooterText>
+          Este site foi criado para democratizar a formação de produtores de
+          conteúdo para SEO que desejam se especializar na área.
+        </FooterText>
+        <FooterSocialIcons>
+          <FooterSocialIcon src={FacebookIcon}></FooterSocialIcon>
+          <FooterSocialIcon src={InstagramIcon}></FooterSocialIcon>
+          <FooterSocialIcon src={TwitterIcon}></FooterSocialIcon>
+          <FooterSocialIcon src={YoutubeIcon}></FooterSocialIcon>
+        </FooterSocialIcons>
+      </FooterIntro>
+
+      {/* Categorias */}
+      <FooterCategories>
+        <FooterCategoryTitle>Categorias</FooterCategoryTitle>
+        <FooterCategory>Como funciona</FooterCategory>
+        <FooterCategory>Quem Somos</FooterCategory>
+        <FooterCategory>Contato</FooterCategory>
+        <FooterCategory>
+          <FooterCategoryBtn>Inscreva-se</FooterCategoryBtn>
+        </FooterCategory>
+      </FooterCategories>
+
+      {/* form */}
+      <FooterFormContainer>
+              <FooterFormTitle>Entre em contato com a gente</FooterFormTitle>
+              <FooterForm>
+              <FooterFormLabel placeholder="Nome"></FooterFormLabel>
+              <FooterFormLabel placeholder="Telefone/Celular"></FooterFormLabel>
+                  <FooterFormLabel placeholder="E-mail"></FooterFormLabel>
+                  <FooterLabelTextArea placeholder="Digite sua mensagem"></FooterLabelTextArea>
+              <FooterFormBtn>Enviar</FooterFormBtn>
+              </FooterForm>
+      </FooterFormContainer>
     </FooterContainer>
   );
 }
--- src/Components/Footer/footer.style.jsx
+++ src/Components/Footer/footer.style.jsx
@@ -2,42 +2,157 @@ import Styled from "styled-components";
 
 export const FooterContainer = Styled.div`
   width:100%;
-  height:fit-content;
+  height:400px;
   background-color: #000000;
+  display: grid;
+  grid-template-columns:1fr 1fr 1fr;
+  justify-items:center;
+  align-items:center;
+
+  @media(max-width:700px){
+  display:grid;
+  grid-template-columns:1fr;
+  height: fit-content;
+  padding-bottom:30px;
+  }
 `;
 
 export const FooterIntro = Styled.div`
  margin:0 auto;
  padding: 20px;
+ @media(max-width:700px){
+    text-align:center;
+    margin-bottom:2rem;
+ }
 `;
 
 export const FooterLogo = Styled.img`
   margin:10px;
   max-width:180px;
   height: auto;
+  
 `;
 
 export const FooterText = Styled.div`
- color:#fff;
+  color:#fff;
   max-width:280px;
   padding:0 10px;
+  font-family:inter;
+  font-size: 15px;
 `;
 
 export const FooterSocialIcons = Styled.div`
   display: flex;
+  justify-content:start;
   gap: 1rem;
   list-style-type: none;
   color:#fff;
   margin-top: 20px;
   padding:0 10px;
+  @media(max-width:700px){
+    margin-bottom:1rem;
+    justify-content:center;
+  }
+`;
+
+export const FooterSocialIcon = Styled.img`
+ background-color:#fff;
+ border-radius:20px;
+ width:40px;
+ transition:.5s;
+ cursor:pointer;
+ &:hover{
+    transform:translateY(2px);
+    opacity:0.7;
+ }
 `;
 
 export const FooterCategories = Styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  text-align:center;
+  @media(max-width:700px){
+    margin-bottom:2rem;
+  }
 `;
 
 export const FooterCategoryTitle = Styled.div`
+font-size:25px;
+font-weight:900;
+margin-bottom:15px;
+  color:#fff;
 `;
 
 export const FooterCategory = Styled.div`
+font-size:15px;
+color:#fff;
+line-height:2rem;
+font-weight:700;
+transition:.5s;
+cursor:pointer;
+
+&:hover{
+    transform: translateX(15px);
+}
 `;
 
+export const FooterCategoryBtn = Styled.button`
+  margin-top: 10px;
+  background-color:#fff;
+  font-weight:700;
+  padding:7px 15px;
+  border-radius:10px;
+  cursor:pointer;
+  &:hover{
+    background-color:transparent;
+    color:#fff;
+  }
+`;
+
+export const FooterFormContainer = Styled.div`
+   max-width:200px;
+`;
+
+export const FooterForm = Styled.div`
+  display: flex;
+  flex-direction: column;
+  gap:1rem;
+`;
+
+export const FooterFormTitle = Styled.h4`
+   color:#fff;
+   font-size:25px;
+   text-align:center;
+   width:fit-content;
+`;
+
+export const FooterFormLabel = Styled.input`
+   padding:5px;
+   width:200px;
+   background-color:#fff;
+   border-radius: 5px;
+`;
+
+export const FooterFormBtn = Styled.div`
+   background-color: #fff;
+   color:#000;
+   font-weight:700;
+   text-align:center;
+   border:1px solid #fff;
+   padding:5px;
+   width:200px;
+   transition:.5s;
+   border-radius: 5px;
+   cursor:pointer;
+   &:hover{
+    background-color:transparent;
+    color:#fff;
+   }
+`;
+
+export const FooterLabelTextArea = Styled.textarea`
+   max-width:210px;
+   border-radius: 5px;
+
+`;
