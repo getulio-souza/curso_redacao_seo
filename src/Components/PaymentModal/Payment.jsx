345c4e448807c69dc2f268d2e2a9e7c9df3a064f
Getúlio Souza <getuliosouza@live.com>
2022-09-07 21:19:45 -0300

    updating credit card

--- src/Components/PaymentModal/Payment.jsx
+++ src/Components/PaymentModal/Payment.jsx
@@ -2,8 +2,26 @@ import React from "react";
 import {
   PaymentModalContainer,
   PaymentModalInternalContainer,
+
+  /* cartão - frente */
+  CreditCardContainer,
+  CreditCardFlagContainer,
+  CreditCardFlag,
+  CreditCardChipAndNumber,
+  CreditCardChip,
+  CreditCardNumber,
+  CreditCardNameAndExpiration,
+  CreditCardName,
+  CreditCardExpiration,
+  /* cartão - verso */
+  CreditCardBack,
+  CreditCardBackStripe,
+  CreditCardBackSecurityCode,
+  CreditCardBackBox,
+  CreditCardBackImage,
+  /*campos */
+  PaymentInfoContainer,
   PaymentBox,
-  PaymentMainTitle,
   PaymentCardNumber,
   PaymentCardNumberInput,
   PaymentCardOwner,
@@ -15,78 +33,106 @@ import {
   PaymentCodeInput,
   PaymentSubmitBtn,
   PaymentPasswordSaveBox,
-  PaymentPasswordSaveText,
-  // PaymentPasswordSaveCheck,
   ClosePaymentBtn,
 } from "./Payment.style";
+
+import Cardflag from "../../assets/credit-card/flag-2.png";
+
+import CardChip from "../../assets/credit-card/chip.jpg";
+
 function PaymentModal({ closeModal }) {
   return (
     <PaymentModalContainer>
       <PaymentModalInternalContainer>
-        <PaymentBox>
-          <PaymentMainTitle>Informe seus dados</PaymentMainTitle>
-          <PaymentCardNumber>Número do cartão</PaymentCardNumber>
-          <PaymentCardNumberInput
-            type="text"
-            maxLength="16"
-          ></PaymentCardNumberInput>
-        </PaymentBox>
-        <PaymentBox>
-          <PaymentCardOwner>Dono do cartão</PaymentCardOwner>
-          <PaymentCardNumberInput
-            type="text"
-            maxLength="16"
-          ></PaymentCardNumberInput>
-        </PaymentBox>
-        <ExpirationMonth>
-          <InputText>Data de expiração</InputText>
-          <InputBoxMonthSelect>
-            <option value="mês" selected disabled>
-              Mês
-            </option>
-            <option value="01">01</option>
-            <option value="02">02</option>
-            <option value="03">03</option>
-            <option value="04">04</option>
-            <option value="05">05</option>
-            <option value="06">06</option>
-            <option value="07">07</option>
-            <option value="08">08</option>
-            <option value="09">09</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-          </InputBoxMonthSelect>
-          <InputBoxMonthSelect>
-            <option value="mês" selected disabled>
-              Ano
-            </option>
-            <option value="2022">2022</option>
-            <option value="2023">2023</option>
-            <option value="2024">2024</option>
-            <option value="2025">2025</option>
-            <option value="2026">2026</option>
-            <option value="2027">2027</option>
-            <option value="2028">2028</option>
-            <option value="2029">2029</option>
-            <option value="2030">2030</option>
-          </InputBoxMonthSelect>
-        </ExpirationMonth>
-        <PaymentCodeBox>
-          <PaymentCodeTitle>CVV</PaymentCodeTitle>
-          <PaymentCodeInput
-            type="text"
-            maxLength="3"
-            pattern="[0-9]+"
-          ></PaymentCodeInput>
-        </PaymentCodeBox>
-        <PaymentPasswordSaveBox>
-          {/* <PaymentPasswordSaveCheck></PaymentPasswordSaveCheck> */}
-          <PaymentPasswordSaveText>Memorizar cartão</PaymentPasswordSaveText>
-          <PaymentSubmitBtn>Confirmar</PaymentSubmitBtn>
-        </PaymentPasswordSaveBox>
-        <ClosePaymentBtn onClick={() => closeModal(false)}>x</ClosePaymentBtn>
+        {/* area do cartão - frente */}
+        <CreditCardContainer>
+          <CreditCardFlagContainer>
+            <CreditCardFlag src={Cardflag}></CreditCardFlag>
+          </CreditCardFlagContainer>
+
+          <CreditCardChipAndNumber>
+            <CreditCardChip src={CardChip}></CreditCardChip>
+            <CreditCardNumber>0000 0000 0000 0000</CreditCardNumber>
+          </CreditCardChipAndNumber>
+
+          <CreditCardNameAndExpiration>
+            <CreditCardName>José Bonifácio Santana</CreditCardName>
+            <CreditCardExpiration>00/00</CreditCardExpiration>
+          </CreditCardNameAndExpiration>
+
+          {/* area do cartão - verso */}
+          <CreditCardBack>
+            <CreditCardBackStripe></CreditCardBackStripe>
+            <CreditCardBackSecurityCode>CVV</CreditCardBackSecurityCode>
+            <CreditCardBackBox></CreditCardBackBox>
+            <CreditCardBackImage src={Cardflag}></CreditCardBackImage>
+          </CreditCardBack>
+        </CreditCardContainer>
+
+        {/* area para preencher dados */}
+        <PaymentInfoContainer>
+          <PaymentBox>
+            <PaymentCardNumber>Número do cartão</PaymentCardNumber>
+            <PaymentCardNumberInput
+              type="text"
+              maxLength="16"
+            ></PaymentCardNumberInput>
+          </PaymentBox>
+          <ExpirationMonth>
+            <InputText>Data de expiração</InputText>
+            <InputBoxMonthSelect>
+              <option value="mês" selected disabled>
+                Mês
+              </option>
+              <option value="01">01</option>
+              <option value="02">02</option>
+              <option value="03">03</option>
+              <option value="04">04</option>
+              <option value="05">05</option>
+              <option value="06">06</option>
+              <option value="07">07</option>
+              <option value="08">08</option>
+              <option value="09">09</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </InputBoxMonthSelect>
+            <InputBoxMonthSelect>
+              <option value="mês" selected disabled>
+                Ano
+              </option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
+              <option value="2026">2026</option>
+              <option value="2027">2027</option>
+              <option value="2028">2028</option>
+              <option value="2029">2029</option>
+              <option value="2030">2030</option>
+            </InputBoxMonthSelect>
+          </ExpirationMonth>
+          <PaymentBox>
+            <PaymentCardOwner>Dono do cartão</PaymentCardOwner>
+            <PaymentCardNumberInput
+              type="text"
+              maxLength="16"
+            ></PaymentCardNumberInput>
+          </PaymentBox>
+          <PaymentCodeBox>
+            <PaymentCodeTitle>CVV</PaymentCodeTitle>
+            <PaymentCodeInput
+              type="text"
+              maxLength="3"
+              pattern="[0-9]+"
+            ></PaymentCodeInput>
+          </PaymentCodeBox>
+          <PaymentPasswordSaveBox>
+            <PaymentSubmitBtn>Confirmar</PaymentSubmitBtn>
+          </PaymentPasswordSaveBox>
+        </PaymentInfoContainer>
       </PaymentModalInternalContainer>
+      <ClosePaymentBtn onClick={() => closeModal(false)}>x</ClosePaymentBtn>
     </PaymentModalContainer>
   );
 }
--- src/Components/PaymentModal/Payment.style.jsx
+++ src/Components/PaymentModal/Payment.style.jsx
@@ -21,12 +21,107 @@ export const PaymentModalContainer = styled.form`
 
 export const PaymentModalInternalContainer = styled.div`
   position: fixed;
-  background-color: #fff;
+  background-color: #d8d4d4;
   padding: 20px;
   border-radius: 20px;
   box-shadow: 2px 3px 4px #000;
   z-index: 43;
 `;
+
+/* Área do cartão - frente */
+
+export const CreditCardContainer = styled.div`
+  background-color: #fff;
+  width: 100%;
+  height: fit-content;
+  margin-bottom: 10px;
+  position: relative;
+  box-shadow: 1px 2px 3px #000;
+  border-radius: 10px;
+  transition: transform 0.4s ease-out;
+`;
+
+export const CreditCardFlagContainer = styled.div`
+  display: flex;
+`;
+
+export const CreditCardFlag = styled.img`
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
+export const CreditCardChipAndNumber = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CreditCardChip = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-left: 10px;
+  margin-top: 10px;
+`;
+
+export const CreditCardNumber = styled.h6`
+  margin:10px 0 0 10px;
+`;
+
+export const CreditCardNameAndExpiration = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 10px;
+`;
+
+export const CreditCardName = styled.h6``;
+
+export const CreditCardExpiration = styled.p`
+  font-size: 10px;
+`;
+
+/* Área do cartão - verso */
+
+export const CreditCardBack = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  box-shadow: 1px 2px 3px #000;
+  border-radius: 5px;
+  text-align:right;
+  transition:transform .4s ease-out;
+`;
+
+export const CreditCardBackStripe = styled.div`
+  background-color:#000;
+  width:100%;
+  margin:10px 0;
+  height:20px;
+`;
+
+export const CreditCardBackBox = styled.div`
+   padding:0 20px;
+`;
+
+export const CreditCardBackSecurityCode = styled.h6`
+   color: #fff;
+   font-size:15px;
+`;
+
+export const CreditCardBackImage = styled.img`
+ width:30px;
+ height:30px;
+
+`;
+
+/* Área de preenchimento de dados **/
+
+export const PaymentInfoContainer = styled.div``;
 
 export const PaymentBox = styled.div`
   display: flex;
@@ -50,7 +145,7 @@ export const PaymentCardOwner = styled.h4`
 
 export const PaymentCardNumberInput = styled.input`
   border: none;
-  border-bottom: 1px solid #000;
+  border-radius: 5px;
 `;
 
 export const InputBoxContainer = styled.div``;
@@ -59,6 +154,7 @@ export const ExpirationMonth = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+  margin-top: 10px;
 `;
 
 export const ExpirationDate = styled.div`
@@ -74,16 +170,22 @@ export const InputText = styled.h5`
 `;
 
 export const InputBoxMonthSelect = styled.select`
-  height: 30px;
+  height: 25px;
   border: none;
-  border-bottom: 1px solid #000;
-  background-color: transparent;
+  background-color: #fff;
   color: #444242;
   font-size: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition:.5s;
+  &:hover{
+    animation-direction: normal;
+  }
 `;
 
 export const ClosePaymentBtn = styled.button`
-  font-size: 20px;
+  color: #fff;
+  font-size: 50px;
   border: none;
   background-color: transparent;
   position: absolute;
@@ -99,9 +201,10 @@ export const ClosePaymentBtn = styled.button`
 
 export const PaymentCodeBox = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 10px;
+  margin-bottom: 20px;
 `;
 
 export const PaymentCodeTitle = styled.h5`
@@ -112,7 +215,7 @@ export const PaymentCodeTitle = styled.h5`
 export const PaymentCodeInput = styled.input`
   width: 50px;
   border: none;
-  border-bottom: 1px solid #000;
+  border-radius: 5px;
 `;
 
 export const PaymentPasswordSaveBox = styled.label`
@@ -126,10 +229,6 @@ export const PaymentPasswordSaveBox = styled.label`
 export const PaymentPasswordSaveText = styled.h6`
   color: #444242;
 `;
-
-// export const PaymentPasswordSaveCheck = styled.button`
-
-// `;
 
 export const PaymentSubmitBtn = styled.button`
   background-color: #000;
