c270bc2b8b60f726b67dd746d43d8b0baa4b6b4c
getulio.souza <getulio.souza@evolutix.com.br>
2022-10-12 17:23:09 -0300

    starting-dashboard

--- src/Components/Editor-page/seo-metrics/seo-metrics.jsx
+++ src/Components/Editor-page/seo-metrics/seo-metrics.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
+import React from "react";
 import {
+  SeoMetricsBackground,
   SeoMetricsContainer,
+  SeoMetricsCardsContainer,
+  SeoMetricsCard,
+  SeoMetricsCardtitle,
+  SeoMetricsCardNumber,
+  SeoMetricsTitleContainer,
+  SeoMetricsCardIcon,
+  SeoMetricsCardPercentage,
   SeoMetricsTitle,
-  SeoMetricsDashboard,
+  SeoMetricsTableContainer,
 } from "./seo-metrics.styles";
-
-import dashboardExemple from "../../../assets/dashboard/figma-admin-dashboard-ui-kit.jpg"
 
 function seoMetrics() {
   return (
-    <SeoMetricsContainer>
-      <SeoMetricsTitle>Métricas de SEO - Dashboard</SeoMetricsTitle>
-      <SeoMetricsDashboard src={dashboardExemple}></SeoMetricsDashboard>
-    </SeoMetricsContainer>
+    <SeoMetricsBackground>
+      <SeoMetricsContainer>
+        <SeoMetricsTitleContainer>
+          <SeoMetricsTitle>Métricas de SEO</SeoMetricsTitle>
+        </SeoMetricsTitleContainer>
+        <SeoMetricsCardsContainer>
+          {/* card*/}
+          <SeoMetricsCard>
+            <SeoMetricsCardIcon></SeoMetricsCardIcon>
+            <SeoMetricsCardtitle>Palavras ranqueadas</SeoMetricsCardtitle>
+            <SeoMetricsCardNumber>20</SeoMetricsCardNumber>
+            <SeoMetricsCardPercentage></SeoMetricsCardPercentage>
+          </SeoMetricsCard>
+          {/* card*/}
+          <SeoMetricsCard>
+            <SeoMetricsCardIcon></SeoMetricsCardIcon>
+            <SeoMetricsCardtitle>
+              Posição nos mecanismos de busca
+            </SeoMetricsCardtitle>
+            <SeoMetricsCardNumber>20</SeoMetricsCardNumber>
+            <SeoMetricsCardPercentage></SeoMetricsCardPercentage>
+          </SeoMetricsCard>
+          {/* card*/}
+          <SeoMetricsCard>
+            <SeoMetricsCardIcon></SeoMetricsCardIcon>
+            <SeoMetricsCardtitle>Palavras ranqueadas</SeoMetricsCardtitle>
+            <SeoMetricsCardNumber>20</SeoMetricsCardNumber>
+            <SeoMetricsCardPercentage></SeoMetricsCardPercentage>
+          </SeoMetricsCard>
+        </SeoMetricsCardsContainer>
+        <SeoMetricsTableContainer></SeoMetricsTableContainer>
+      </SeoMetricsContainer>
+    </SeoMetricsBackground>
   );
 }
 
-export default seoMetrics
+export default seoMetrics;
--- src/Components/Editor-page/seo-metrics/seo-metrics.styles.jsx
+++ src/Components/Editor-page/seo-metrics/seo-metrics.styles.jsx
@@ -1,28 +1,65 @@
 import styled from "styled-components";
+export const SeoMetricsBackground = styled.div`
+  background-color: #000;
+width: 100%;
+height: 100vh;
+`;
 
 export const SeoMetricsContainer = styled.div`
-  background-color: #000;
-  display: flex;
-  flex-direction:column;
-  justify-content: center;
-  align-items: center;
+  background-color: #fff;
+  border-radius:10px;
+  box-shadow:1px 1px 1px #c8c4c4;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 100%;
-  height: 100vh;
-  font-family:inter;
+  font-family: inter;
+  width:900px;
+  height:450px;
+  @media screen and (min-width:1366px){
+    height:500px;
+  }
 `;
+export const SeoMetricsTitleContainer = styled.div`
+  padding: 10px 0;
+`;
+
+
 
 export const SeoMetricsTitle = styled.h1`
-  background-color: #000;
-  color: #fff;
-  padding: 10px 15px;
-  font-weight: 700;
+ color:#000;
+ text-align:center;
 `;
 
-export const SeoMetricsDashboard = styled.img`
-   width:50%;
-   height:auto;
+export const SeoMetricsCardsContainer = styled.div`
+  display: flex;
+  justify-content:center;
+  align-items:center;
+  gap:40px;
+`;
+
+export const SeoMetricsCard = styled.div`
+    background-color: #000;
+    padding: 10px;
+    min-width:220px;
+    min-height:110px;
+    border-radius:20px;
+`;
+
+export const SeoMetricsCardIcon = styled.div``;
+
+export const SeoMetricsCardtitle = styled.h5`
+  color:#fff;
+  text-align:center;
+  max-width:150px;
+  margin: 0 auto;
+  margin-top: 10px;
+`;
+export const SeoMetricsCardNumber = styled.div``;
+export const SeoMetricsCardPercentage = styled.div``;
+
+export const SeoMetricsTableContainer = styled.div`
+  width:100%;
+  height: auto;
+  background-color: #000;
 `;
