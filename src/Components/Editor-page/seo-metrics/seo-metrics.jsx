452f21f21893f2be4595a689a2f418ab6eab9c3a
getulio.souza <getulio.souza@evolutix.com.br>
2022-10-27 09:45:59 -0300

    Revert "Revert "table-add""
    This reverts commit e9aa2ea2720136009524b58e1995439313d42b92.

--- src/Components/Editor-page/seo-metrics/seo-metrics.jsx
+++ src/Components/Editor-page/seo-metrics/seo-metrics.jsx
@@ -11,6 +11,10 @@ import {
   SeoMetricsCardPercentage,
   SeoMetricsTitle,
   SeoMetricsTableContainer,
+  SeoMetricsTableRow,
+  SeoMetricsTableHead,
+  SeoMetricsTableData,
+  SeoMetricsTableDataBtn,
 } from "./seo-metrics.styles";
 
 function seoMetrics() {
@@ -34,18 +38,58 @@ function seoMetrics() {
             <SeoMetricsCardtitle>
               Posição nos mecanismos de busca
             </SeoMetricsCardtitle>
-            <SeoMetricsCardNumber>20</SeoMetricsCardNumber>
+            <SeoMetricsCardNumber>2ª</SeoMetricsCardNumber>
             <SeoMetricsCardPercentage></SeoMetricsCardPercentage>
           </SeoMetricsCard>
           {/* card*/}
           <SeoMetricsCard>
             <SeoMetricsCardIcon></SeoMetricsCardIcon>
-            <SeoMetricsCardtitle>Palavras ranqueadas</SeoMetricsCardtitle>
-            <SeoMetricsCardNumber>20</SeoMetricsCardNumber>
+            <SeoMetricsCardtitle>Tempo de permanência</SeoMetricsCardtitle>
+            <SeoMetricsCardNumber>4min20s</SeoMetricsCardNumber>
             <SeoMetricsCardPercentage></SeoMetricsCardPercentage>
           </SeoMetricsCard>
         </SeoMetricsCardsContainer>
-        <SeoMetricsTableContainer></SeoMetricsTableContainer>
+        {/* table */}
+        <SeoMetricsTableContainer>
+          <SeoMetricsTableRow>
+            <SeoMetricsTableHead>Autor</SeoMetricsTableHead>
+            <SeoMetricsTableHead>Texto</SeoMetricsTableHead>
+            <SeoMetricsTableHead>quantidade de palavas</SeoMetricsTableHead>
+            <SeoMetricsTableHead>Ação</SeoMetricsTableHead>
+          </SeoMetricsTableRow>
+          <SeoMetricsTableRow>
+            <SeoMetricsTableData>José</SeoMetricsTableData>
+            <SeoMetricsTableData>Texto 1</SeoMetricsTableData>
+            <SeoMetricsTableData>visualizar</SeoMetricsTableData>
+            <SeoMetricsTableData>
+              <SeoMetricsTableDataBtn>ver mais</SeoMetricsTableDataBtn>
+            </SeoMetricsTableData>
+          </SeoMetricsTableRow>
+          <SeoMetricsTableRow>
+            <SeoMetricsTableData>José</SeoMetricsTableData>
+            <SeoMetricsTableData>Texto 2</SeoMetricsTableData>
+            <SeoMetricsTableData>850</SeoMetricsTableData>
+            <SeoMetricsTableData>
+              <SeoMetricsTableDataBtn>ver mais</SeoMetricsTableDataBtn>
+            </SeoMetricsTableData>
+          </SeoMetricsTableRow>
+          <SeoMetricsTableRow>
+            <SeoMetricsTableData>José</SeoMetricsTableData>
+            <SeoMetricsTableData>Texto 3</SeoMetricsTableData>
+            <SeoMetricsTableData>670</SeoMetricsTableData>
+            <SeoMetricsTableData>
+              <SeoMetricsTableDataBtn>ver mais</SeoMetricsTableDataBtn>
+            </SeoMetricsTableData>
+          </SeoMetricsTableRow>
+          <SeoMetricsTableRow>
+            <SeoMetricsTableData>José</SeoMetricsTableData>
+            <SeoMetricsTableData>Texto 4</SeoMetricsTableData>
+            <SeoMetricsTableData>1210</SeoMetricsTableData>
+            <SeoMetricsTableData>
+              <SeoMetricsTableDataBtn>ver mais</SeoMetricsTableDataBtn>
+            </SeoMetricsTableData>
+          </SeoMetricsTableRow>
+        </SeoMetricsTableContainer>
       </SeoMetricsContainer>
     </SeoMetricsBackground>
   );
--- src/Components/Editor-page/seo-metrics/seo-metrics.styles.jsx
+++ src/Components/Editor-page/seo-metrics/seo-metrics.styles.jsx
@@ -1,65 +1,102 @@
 import styled from "styled-components";
 export const SeoMetricsBackground = styled.div`
-  background-color: #000;
-width: 100%;
-height: 100vh;
+  width: 100%;
+  height: 100vh;
 `;
 
 export const SeoMetricsContainer = styled.div`
   background-color: #fff;
-  border-radius:10px;
-  box-shadow:1px 1px 1px #c8c4c4;
+  border-radius: 10px;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   font-family: inter;
-  width:900px;
-  height:450px;
-  @media screen and (min-width:1366px){
-    height:500px;
-  }
+  width: 900px;
+  height: fit-content;
+  padding-bottom: 20px;
 `;
 export const SeoMetricsTitleContainer = styled.div`
   padding: 10px 0;
 `;
 
-
-
 export const SeoMetricsTitle = styled.h1`
- color:#000;
- text-align:center;
+  color: #000;
+  text-align: center;
 `;
 
 export const SeoMetricsCardsContainer = styled.div`
   display: flex;
-  justify-content:center;
-  align-items:center;
-  gap:40px;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
 `;
 
 export const SeoMetricsCard = styled.div`
-    background-color: #000;
-    padding: 10px;
-    min-width:220px;
-    min-height:110px;
-    border-radius:20px;
+  background-color: #000;
+  padding: 10px;
+  min-width: 220px;
+  min-height: 110px;
+  border-radius: 20px;
 `;
 
 export const SeoMetricsCardIcon = styled.div``;
 
 export const SeoMetricsCardtitle = styled.h5`
-  color:#fff;
-  text-align:center;
-  max-width:150px;
+  color: #fff;
+  text-align: center;
+  max-width: 150px;
   margin: 0 auto;
   margin-top: 10px;
 `;
-export const SeoMetricsCardNumber = styled.div``;
+export const SeoMetricsCardNumber = styled.div`
+  margin-top: 10px;
+  font-size: 40px;
+  color: #fff;
+  text-align: center;
+`;
 export const SeoMetricsCardPercentage = styled.div``;
 
-export const SeoMetricsTableContainer = styled.div`
-  width:100%;
-  height: auto;
+export const SeoMetricsTableContainer = styled.table`
+  margin: 0 auto;
+  margin-top: 30px;
+  text-align: center;
   background-color: #000;
+  padding: 10px;
+`;
+
+export const SeoMetricsTableRow = styled.tr`
+  padding-bottom: 10px;
+`;
+
+export const SeoMetricsTableHead = styled.th`
+  padding: 0 50px;
+  color: #fff;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 30px;
+  border: 1px solid #fff;
+`;
+
+export const SeoMetricsTableData = styled.td`
+  border: 1px solid #fff;
+  border-top: none;
+  color: #fff;
+  font-size: 15px;
+`;
+
+export const SeoMetricsTableDataBtn = styled.button`
+  border: none;
+  margin: 5px 0;
+  padding: 5px 10px;
+  background-color: #fff;
+  color: #000;
+  border-radius: 5px;
+  transition: 0.5s;
+  cursor: pointer;
+  &:hover {
+    background-color: transparent;
+    border: 1px solid #fff;
+    color: #fff;
+  }
 `;
