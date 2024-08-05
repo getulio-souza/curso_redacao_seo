57bebfb66597bdc794acb54b5986b23879e5a81a
getulio.souza <getulio.souza@evolutix.com.br>
2024-08-05 11:31:19 -0300

    bugfix: input-telefone-subscribe-modal

--- src/Components/SubscribeModal/subscribeModal.jsx
+++ src/Components/SubscribeModal/subscribeModal.jsx
@@ -15,9 +15,28 @@ import {
 import ModalImg from "../../assets/modal/modal_btn_subscribe.jpg";
 import LoginModal from "../LoginModal/loginModal";
 
+//formatando numero de telefone para o padrao (99) 99999-9999
+const formatPhoneNumber = (value) => {
+  const cleaned = value.replace(/\D/g, "");
+  if (cleaned.length <= 2) {
+    return `${cleaned}`
+  }
+  if (cleaned.length <= 6) {
+    return `(${cleaned.slice(0,2)}) ${cleaned.slice(2)}`
+  }
+  return `(${cleaned.slice(0,2)}) ${cleaned.slice(2,7)}-${cleaned.slice(7,11)}`
+}
+
 function SubscribeModal({ closeModal }) {
   const [OpenLoginModal, setOpenLoginModal] = useState(false);
   const [OpenSubscribeModal, SetOpenSubscribeModal] = useState(false);
+
+  //inputs validations
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handlePhoneNumber = (e) => {
+    setPhoneNumber(formatPhoneNumber(e.target.value));
+  };
 
   return (
     <ModalContainer>
@@ -36,7 +55,15 @@ function SubscribeModal({ closeModal }) {
         <ModalRightColumnForm>
           <ModalRightColumnInput placeholder="Nome"></ModalRightColumnInput>
           <ModalRightColumnInput placeholder="E-mail"></ModalRightColumnInput>
-          <ModalRightColumnInput placeholder="Telefone"></ModalRightColumnInput>
+          <ModalRightColumnInput
+            placeholder="(xx) xxxx-xxx"
+            type="tel"
+            id="phone"
+            value={phoneNumber}
+            onChange={handlePhoneNumber}
+            pattern="[0-9]*"
+            maxLength="15"
+          ></ModalRightColumnInput>
           <ModalRightColumnInput placeholder="Senha"></ModalRightColumnInput>
         </ModalRightColumnForm>
         {/* subscribe button */}
